4fae19de18805bf76fd576fcd253041400a7f342
YSY <313008602@qq.com>
2017-02-21 15:14:16 +0800

    a

---
+++ QueryCloudFeatureService/gen/com/arcgis/android/tutorials/querycloudfeatureservice/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.arcgis.android.tutorials.querycloudfeatureservice;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
